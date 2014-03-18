0b4ba3ac8e8f6aa3d413b270fc71994bfadf814b
Alec S <linuxbash8@gmail.com>
2014-03-18 16:19:30 -0500

    minor changes, check release

--- install.sh
+++ install.sh
@@ -15,17 +15,15 @@ if [[ ! -e ~/gdrive ]]
 then
     mkdir ~/gdrive
 fi
-sudo cp *.java /lib/gdrive
 sudo cp gdrived.c /lib/gdrive
 sudo cp gdrive /bin/gdrive
 sudo cp gdrive-libs.zip /lib/gdrive
-sudo cp EasyReader.class /lib/gdrive
-sudo cp EasyWriter.class /lib/gdrive
+sudo cp *.class /lib/gdrive
+sudo cp drive.jpg /lib/gdrive
 sudo cp google-drive.desktop /usr/share/applications
 cp driveXml.xml ~/gdrive/.drive.xml
 cd /lib/gdrive
 sudo gcc gdrived.c -o gdrived
 sudo mv gdrived /bin/gdrived
 sudo unzip /lib/gdrive/gdrive-libs.zip
-sudo javac -cp ".:./*:/lib/gdrive/*:/lib/gdrive/drive/*:/lib/gdrive/drive/libs/*" *.java
 sudo chmod +x /bin/gdrive
